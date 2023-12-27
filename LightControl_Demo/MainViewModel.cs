5fd5b858ba5d7c73dc2eacc372a5e33e9e9dfe79
JamesBaiJun <42957381+JamesBaiJun@users.noreply.github.com>
2023-12-27 17:48:22 +0800

    为导航项添加Command

---
+++ LightControl_Demo/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using DevExpress.Mvvm.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightControl_Demo
+{
+    [POCOViewModel]
+    public class MainViewModel
+    {
+        public void Test(object content)
+        {
+            Debug.WriteLine(content.ToString());
+        }
+    }
+}
